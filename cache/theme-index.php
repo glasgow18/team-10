4e53703a9e7f09e36c95385038130b939f9f2a56
Chris Connor <cjconnor24@connordesign.com>
2018-10-27 21:01:45 +0100

    Trying to remove the vanilla files

--- cache/theme-index.php
+++
@@ -1,7 +0,0 @@
-<?php return array (
-  '2011Compatibility' => '/themes/2011Compatibility',
-  'bittersweet' => '/themes/bittersweet',
-  'default' => '/themes/default',
-  'EmbedFriendly' => '/themes/EmbedFriendly',
-  'mobile' => '/themes/mobile',
-);
