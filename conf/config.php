b5d5efdbe0f1659f48ffdb2f3531d3018d6a326f
Chris Connor <cjconnor24@connordesign.com>
2018-10-27 17:49:30 +0100

    Initial commit of the Vanilla Forum files 🎉

---
+++ conf/config.php
@@ -0,0 +1,48 @@
+<?php if (!defined('APPLICATION')) exit();
+
+// Conversations
+$Configuration['Conversations']['Version'] = '2.4.201';
+
+// Database
+$Configuration['Database']['Name'] = 'cwc';
+$Configuration['Database']['Host'] = 'localhost';
+$Configuration['Database']['User'] = 'cwc_user';
+$Configuration['Database']['Password'] = '[p]S?2rS';
+
+// EnabledApplications
+$Configuration['EnabledApplications']['Conversations'] = 'conversations';
+$Configuration['EnabledApplications']['Vanilla'] = 'vanilla';
+
+// EnabledPlugins
+$Configuration['EnabledPlugins']['recaptcha'] = true;
+$Configuration['EnabledPlugins']['GettingStarted'] = 'GettingStarted';
+$Configuration['EnabledPlugins']['stubcontent'] = true;
+$Configuration['EnabledPlugins']['swagger-ui'] = true;
+
+// Garden
+$Configuration['Garden']['Title'] = 'Children with Cancer';
+$Configuration['Garden']['Cookie']['Salt'] = 'gXOjRi8uBOLnhS0E';
+$Configuration['Garden']['Cookie']['Domain'] = '';
+$Configuration['Garden']['Registration']['ConfirmEmail'] = true;
+$Configuration['Garden']['Email']['SupportName'] = 'Children with Cancer';
+$Configuration['Garden']['Email']['Format'] = 'text';
+$Configuration['Garden']['SystemUserID'] = '1';
+$Configuration['Garden']['InputFormatter'] = 'Markdown';
+$Configuration['Garden']['Version'] = 'Undefined';
+$Configuration['Garden']['CanProcessImages'] = true;
+$Configuration['Garden']['Installed'] = true;
+
+// Plugins
+$Configuration['Plugins']['GettingStarted']['Dashboard'] = '1';
+
+// Routes
+$Configuration['Routes']['YXBwbGUtdG91Y2gtaWNvbi5wbmc='] = array (
+  0 => 'utility/showtouchicon',
+  1 => 'Internal',
+);
+$Configuration['Routes']['DefaultController'] = 'discussions';
+
+// Vanilla
+$Configuration['Vanilla']['Version'] = '2.4.201';
+
+// Last edited by cwcadmin (127.0.0.1) 2018-10-27 16:48:50
