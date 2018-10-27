c0b738b4ebcd07bef3c7972d547366a51dcf221b
Chris Connor <cjconnor24@connordesign.com>
2018-10-28 00:09:27 +0100

    Added some additional error checking / validation

--- create-post.php
+++ create-post.php
@@ -1,31 +1,101 @@
 <?php
 include('includes/db-connect.php');
 
+// RETURN JSON
+header('Content-type: application/json');
 
 // $_POST
-$postcontent = "This is a new post from PHP here is a new onw";
+$postcontent = (isset($_POST['postcontent']) ? $_POST['postcontent'] : '');
 
 // TODO THIS WILL BE DYANMIC DEPENDING ON THE CATEGORY
-$category_id = 1;
+$categoryID = (isset($_POST['categoryID']) ?  $_POST['categoryID'] : "");
 
 // TODO THIS WOULD COME FROM THE SESSION
-$user_id = 1;
+$userID = (isset($_POST['userID']) ? $_POST['userID'] : '');
 
-// TODO - DO SOME CHECKS
+// POST PAERTN
+$postParent = (isset($_POST['postParent']) ? $_POST['postParent'] : '');
 
-// MAKE SURE ISNT BLANK
+// CREATE A CONTAINER FOR ANY ERRROS;
+$errors = [];
 
-// CHECK IF IT'S A REPLY OR A MAIN POST
+// CHECK IF POST IS BLANK
+if($postcontent=="" || !isset($postcontent)){
+
+    $errors[] = "Your post cannot be blank";
+
+}
+
+// CHECK USERID ISNT BLANK
+if($userID=="" || !isset($userID)){
+
+    $errors[] = "There is no user associated with this post";
+
+}
+
+// CHECK IF CAT IS PRESENT
+if($categoryID=="" || !isset($categoryID)){
+
+    $errors[] = "There is no category associated with this post";
+
+}
 
 
 
-$prequery = "INSERT INTO posts (content,created_at,user_id,category_id) values(?,NOW(),?,?)";
+if(count($errors)>0){
 
-$query = $conn->prepare($prequery);
-$query->bindParam(1, $postcontent);
-$query->bindParam(2, $user_id);
-$query->bindParam(3, $category_id);
-$query->execute();
+    $response = array("status"=>"error",'errors'=>$errors);
+    echo json_encode($response);
+
+} else {
+    
+    
+    // TODO: CHECK IF PARENT / CHILD POST
+    if($postParent!=""){
+
+        // DO SOMETHING HERE
+        $prequery = "INSERT INTO posts (content,created_at,user_id,category_id, parent_post) values(?,NOW(),?,?,?)";
+    
+        $query = $conn->prepare($prequery);
+        $query->bindParam(1, $postcontent);
+        $query->bindParam(2, $userID);
+        $query->bindParam(3, $categoryID);        
+        $query->bindParam(4, $postParent);                
+
+    } else {
+
+        $prequery = "INSERT INTO posts (content,created_at,user_id,category_id) values(?,NOW(),?,?)";
+        
+        $query = $conn->prepare($prequery);
+        $query->bindParam(1, $postcontent);
+        $query->bindParam(2, $userID);
+        $query->bindParam(3, $categoryID);
+
+    }
+    
+
+    try {
+
+        $query->execute();
+
+        $response = array('status'=>'success','message'=>'The post was successfully created');
+        echo json_encode($response);
+
+    } catch(PDOException $e){
+
+        print_r($e);
+
+        $response = array('status'=>'error','errors'=>array('There was an issue adding your post.'));
+        echo json_encode($response);
+
+    }
+
+
+    
+
+}
+
+
 
 
 ?>
